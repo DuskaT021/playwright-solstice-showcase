01f4c2beb58e601ee17b3725afa0e022aaba2f4b
DuskaT021 <aleksa2601@gmail.com>
2026-03-18 17:38:38 +0100

    fix: eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,29 @@
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-export default tseslint.config(
+export default [
   {
-    ignores: ["node_modules/**", "playwright-report/**", "test-results/**", ".npm-cache/**"],
+    ignores: [
+      "node_modules/**",
+      "playwright-report/**",
+      "test-results/**",
+      ".npm-cache/**",
+    ],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
     rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "no-undef": "off",
       "@typescript-eslint/consistent-type-imports": [
         "error",
         {
@@ -25,5 +38,5 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "off",
     },
   },
-  eslintConfigPrettier,
-);
+  eslintConfigPrettier
+];
